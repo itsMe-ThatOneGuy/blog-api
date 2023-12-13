1186a376d0fe158c820198cbb4deee2db74c7f61
Matthew Smart <matthewsmart13@gmail.com>
2023-12-12 21:31:57 -0600

    Created basic post model

---
+++ models/post.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+	user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+	title: { type: String, required: true },
+	body: { type: String, required: true },
+	comments: [{ type: Schema.Types.ObjectId, ref: 'Commentj' }],
+	published: { type: Boolean, default: false },
+	postDate: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Post', PostSchema);
