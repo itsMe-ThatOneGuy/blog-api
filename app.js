09819f903211755b590f0c8329279b21ca991fd0
Matthew Smart <matthewsmart13@gmail.com>
2023-12-13 10:39:37 -0600

    Added routes and removed / commented out unused code

--- app.js
+++ app.js
@@ -1,39 +1,34 @@
-const createError = require('http-errors');
+//const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const routes = require('./routes/index');
 
 const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/user', routes.users);
 app.use('/posts', routes.posts);
+app.use('/comments', routes.comments);
 
-// catch 404 and forward to error handler
+/* // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
-
+*/
 module.exports = app;
