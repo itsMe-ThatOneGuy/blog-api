4dd812c5c418ae0bd9b57018aad398fa30e475d2
Matthew Smart <matthewsmart13@gmail.com>
2023-12-14 16:08:50 -0600

    Updated post routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controllers = require('../controllers/indes');
+const controllers = require('../controllers/index');
 
 router.get('/', controllers.postController.get_all_posts);
 
@@ -18,15 +18,23 @@ router.get(
 	controllers.commentController.get_single_comment,
 );
 
-router.post('/:postId/comments', controllers.commentController.post_comment);
+router.post(
+	'/:postId/comments',
+	controllers.commentController.post_create_comment,
+);
 
 router.put('/:postId', controllers.postController.put_update_post);
+
+router.put(
+	'/:postId/comments/:commentId',
+	controllers.commentController.put_update_comment,
+);
 
 router.delete('/:postId', controllers.postController.delete_post);
 
 router.delete(
 	'/:postId/comments/:commentId',
-	controllers.postController.delete_comment,
+	controllers.commentController.delete_comment,
 );
 
 module.exports = router;
