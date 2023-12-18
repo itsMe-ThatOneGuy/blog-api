315e718656ef5cd41612ac484f2e7fdbc76a9b40
Matthew Smart <matthewsmart13@gmail.com>
2023-12-18 09:51:59 -0600

    Created an index file for controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const postController = require('./postController');
 const commentController = require('./commentController');
 const userController = require('./userController');
+const authController = require('./authController');
 
 module.exports = {
 	postController,
 	commentController,
 	userController,
+	authController,
 };
