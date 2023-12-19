7882e52d8411ede90df32bc1d1d4f04afac52ae9
Matthew Smart <matthewsmart13@gmail.com>
2023-12-18 18:56:55 -0600

    Added is admin

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
 	username: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
+	isAdmin: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('User', UserSchema);
