678af6f5800e08cd227ee0fc4f0e836e23a46d68
Matthew Smart <matthewsmart13@gmail.com>
2023-12-20 11:32:12 -0600

    Added id type checking and updated res

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,15 @@ const models = require('../models/index');
 const asyncHandler = require('express-async-handler');
 
 exports.get_user = asyncHandler(async (req, res) => {
-	models.User.findById(req.params.id);
-	return res.json({ id: user._id, username: username });
+	if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+		return res
+			.status(400)
+			.json({ statusCode: 400, message: 'USER ID IS NOT VALID' });
+	}
+	models.User.findById(req.params.userId);
+	return res.status(200).json({
+		statusCode: 200,
+		message: 'SELECTED USER',
+		user: { id: user._id, username: username },
+	});
 });
