8f3d486b2f28aabd5046f454641252f622bcc2d6
Matthew Smart <matthewsmart13@gmail.com>
2023-12-21 15:29:32 -0600

    Removed authentication from individual routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,49 +2,42 @@ const models = require('../models/index');
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const passport = require('passport');
 
-exports.test_auth = [
-	passport.authenticate('jwt', { session: false }),
-	(req, res) => {
+exports.test_auth = (req, res) => {
+	return res
+		.status(200)
+		.json({ statusCode: 200, message: 'AUTH WORKED', user: req.user });
+};
+
+exports.refresh = asyncHandler(async (req, res) => {
+	const refreshToken = req.cookies.jwt;
+	if (!refreshToken) {
 		return res
-			.status(200)
-			.json({ statusCode: 200, message: 'AUTH WORKED', user: req.user });
-	},
-];
+			.status(401)
+			.json({ statusCode: 401, message: 'ACCESS DENIED, NO REFRESH TOKEN' });
+	}
 
-exports.refresh = [
-	passport.authenticate('refresh', { session: false }),
-	asyncHandler(async (req, res) => {
-		const refreshToken = req.cookies.jwt;
-		if (!refreshToken) {
-			return res
-				.status(401)
-				.json({ statusCode: 401, message: 'ACCESS DENIED, NO REFRESH TOKEN' });
-		}
-
-		try {
-			const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_KEY);
-			const payload = {
-				sub: decoded.sub,
-				username: decoded.username,
-				isAdmin: decoded.isAdmin,
-			};
-			const accessToken = jwt.sign(payload, process.env.JWT_TOKEN_KEY, {
-				expiresIn: 120,
-			});
-			res.status(200).json({
-				statusCode: 200,
-				message: 'ACCESS TOKEN REFRESHED',
-				token: accessToken,
-			});
-		} catch {
-			return res
-				.status(400)
-				.json({ statusCode: 400, message: 'Invalid refresh token' });
-		}
-	}),
-];
+	try {
+		const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_KEY);
+		const payload = {
+			sub: decoded.sub,
+			username: decoded.username,
+			isAdmin: decoded.isAdmin,
+		};
+		const accessToken = jwt.sign(payload, process.env.JWT_TOKEN_KEY, {
+			expiresIn: 120,
+		});
+		res.status(200).json({
+			statusCode: 200,
+			message: 'ACCESS TOKEN REFRESHED',
+			token: accessToken,
+		});
+	} catch {
+		return res
+			.status(400)
+			.json({ statusCode: 400, message: 'Invalid refresh token' });
+	}
+});
 
 exports.register_user = asyncHandler(async (req, res) => {
 	bcrypt.hash(req.body.password, 13, async (err, hashedPassword) => {
