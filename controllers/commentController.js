60184dcea3bcc2f73718336a00c6bbf0168090e5
Matthew Smart <matthewsmart13@gmail.com>
2023-12-22 12:04:53 -0600

    Fixed controller structure

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -20,23 +20,21 @@ exports.get_post_comments = asyncHandler(async (req, res) => {
 	});
 });
 
-exports.create_comment = [
-	asyncHandler(async (req, res) => {
-		const comment = new models.Comment({
-			user: req.user.sub,
-			body: req.body.body,
-		});
-		await comment.save();
+exports.create_comment = asyncHandler(async (req, res) => {
+	const comment = new models.Comment({
+		user: req.user.sub,
+		body: req.body.body,
+	});
+	await comment.save();
 
-		const post = await models.Post.findById(req.params.postId);
-		post.comments.push(comment);
-		const updatedPost = await post.save();
+	const post = await models.Post.findById(req.params.postId);
+	post.comments.push(comment);
+	const updatedPost = await post.save();
 
-		return res
-			.status(200)
-			.json({ statusCode: 200, comment: comment, updatedPost: updatedPost });
-	}),
-];
+	return res
+		.status(200)
+		.json({ statusCode: 200, comment: comment, updatedPost: updatedPost });
+});
 
 exports.get_single_comment = asyncHandler(async (req, res) => {
 	const comment = await models.Comment.findById(req.params.commentId);
@@ -52,62 +50,59 @@ exports.get_single_comment = asyncHandler(async (req, res) => {
 		.json({ statusCode: 200, message: 'GET SINGLE COMMENT', comment: comment });
 });
 
-exports.update_comment = [
-	asyncHandler(async (req, res) => {
-		const comment = await models.Comment.findById(req.params.commentId);
+exports.update_comment = asyncHandler(async (req, res) => {
+	const comment = await models.Comment.findById(req.params.commentId);
 
-		if (comment === null) {
-			return res
-				.status(404)
-				.json({ statusCode: 404, message: 'COULD NOT FIND COMMENT' });
-		}
+	if (comment === null) {
+		return res
+			.status(404)
+			.json({ statusCode: 404, message: 'COULD NOT FIND COMMENT' });
+	}
 
-		if (comment.user.id === req.user.sub) {
-			const update = await models.Comment.findByIdAndUpdate(
-				req.params.commentId,
-				{ $set: { body: req.body.body } },
-				{ new: true },
-			);
-			return res
-				.status(200)
-				.json({ statusCode: 200, message: 'UPDATE COMMENT', comment: update });
-		} else {
-			return res
-				.status(403)
-				.json({ statusCode: 403, message: 'NOT AUTHORIZED TO UPDATE' });
-		}
-	}),
-];
-
-exports.delete_comment = [
-	asyncHandler(async (req, res) => {
-		const comment = await models.Comment.findById(
+	if (comment.user.id === req.user.sub) {
+		const update = await models.Comment.findByIdAndUpdate(
 			req.params.commentId,
-		).populate('user', 'id');
+			{ $set: { body: req.body.body } },
+			{ new: true },
+		);
+		return res
+			.status(200)
+			.json({ statusCode: 200, message: 'UPDATE COMMENT', comment: update });
+	} else {
+		return res
+			.status(403)
+			.json({ statusCode: 403, message: 'NOT AUTHORIZED TO UPDATE' });
+	}
+});
 
-		if (comment === null) {
-			return res
-				.status(404)
-				.json({ statusCode: 404, message: 'COULD NOT FIND COMMENT' });
-		}
+exports.delete_comment = asyncHandler(async (req, res) => {
+	const comment = await models.Comment.findById(req.params.commentId).populate(
+		'user',
+		'id',
+	);
 
-		if (comment.user.id === req.user.sub || req.user.isAdmin) {
-			await models.Comment.findByIdAndDelete(req.params.commentId);
-			await models.Post.findOneAndUpdate(
-				{ _id: req.params.postId },
-				{
-					$pull: {
-						comments: req.params.commentId,
-					},
+	if (comment === null) {
+		return res
+			.status(404)
+			.json({ statusCode: 404, message: 'COULD NOT FIND COMMENT' });
+	}
+
+	if (comment.user.id === req.user.sub || req.user.isAdmin) {
+		await models.Comment.findByIdAndDelete(req.params.commentId);
+		await models.Post.findOneAndUpdate(
+			{ _id: req.params.postId },
+			{
+				$pull: {
+					comments: req.params.commentId,
 				},
-			);
-			return res
-				.status(200)
-				.json({ statusCode: 200, message: 'DELETE COMMENT', comment: comment });
-		} else {
-			return res
-				.status(403)
-				.json({ statusCode: 403, message: 'NOT AUTHORIZED TO DELETE COMMENT' });
-		}
-	}),
-];
+			},
+		);
+		return res
+			.status(200)
+			.json({ statusCode: 200, message: 'DELETE COMMENT', comment: comment });
+	} else {
+		return res
+			.status(403)
+			.json({ statusCode: 403, message: 'NOT AUTHORIZED TO DELETE COMMENT' });
+	}
+});
