d917d1bc5acc0b43d2c54c8ae6b2d668e9123c02
Matthew Smart <matthewsmart13@gmail.com>
2023-12-24 02:41:02 -0600

    Created error index for import / export of custom errors

--- middleware/errors/index.js
+++ middleware/errors/index.js
@@ -1,7 +1,9 @@
 const ReqTypeError = require('./ReqTypeError');
 const ResourceError = require('./ResourceError');
+const BadUri = require('./BadUri');
 
 module.exports = {
 	ReqTypeError,
 	ResourceError,
+	BadUri,
 };
