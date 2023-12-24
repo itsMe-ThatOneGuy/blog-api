7e104883a81aa012103209036f8aa5e8333c1685
Matthew Smart <matthewsmart13@gmail.com>
2023-12-24 11:41:13 -0600

    Renamed bad uri error / reordered middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const ErrorHandler = require('./middleware/ErrorHandler');
+
 const errors = require('./middleware/errors/index');
 const models = require('./models/index');
 const routes = require('./routes/index');
@@ -20,19 +21,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 initPassport(passport);
+app.use('/public', routes.public);
 app.use(
 	'/private',
 	passport.authenticate('jwt', { session: false }),
 	routes.private,
 );
-app.use('/public', routes.public);
 app.use(
 	'/token',
 	passport.authenticate('refresh', { session: false }),
 	routes.refresh,
 );
 app.use((req, res, next) => {
-	next(new errors.BadUri());
+	next(new errors.BadUriError());
 });
 app.use(ErrorHandler);
 
