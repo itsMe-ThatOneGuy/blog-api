e1a099aac11a2de11a36659a3c679a9cecf1f28b
Matthew Smart <matthewsmart13@gmail.com>
2023-12-24 11:42:26 -0600

    Renamed bad uri error / Added permission error

--- middleware/errors/index.js
+++ middleware/errors/index.js
@@ -1,9 +1,11 @@
 const ReqTypeError = require('./ReqTypeError');
 const ResourceError = require('./ResourceError');
-const BadUri = require('./BadUri');
+const BadUriError = require('./BadUriError');
+const PermissionError = require('./PermissionError');
 
 module.exports = {
 	ReqTypeError,
 	ResourceError,
-	BadUri,
+	BadUriError,
+	PermissionError,
 };
