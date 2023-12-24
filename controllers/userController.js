eb2d7e105ed5f7d6c0dc05b9d639aa0f08b75034
Matthew Smart <matthewsmart13@gmail.com>
2023-12-24 11:46:01 -0600

    Added the register user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,20 @@
 const models = require('../models/index');
 const asyncHandler = require('express-async-handler');
 
+exports.register_user = asyncHandler(async (req, res, next) => {
+	bcrypt.hash(req.body.password, 13, async (err, hashedPassword) => {
+		if (err) return next(err);
+		const newUser = new models.User({
+			username: req.body.username,
+			password: hashedPassword,
+		});
+		await newUser.save();
+		res
+			.status(200)
+			.json({ statusCode: 200, message: 'User created', user: newUser });
+	});
+});
+
 exports.get_user = asyncHandler(async (req, res) => {
 	await models.User.findById(req.params.userId);
 	return res.status(200).json({
