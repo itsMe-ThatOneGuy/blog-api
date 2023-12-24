a1cc364cae77ba74b289763d80037be3715d8a7a
Matthew Smart <matthewsmart13@gmail.com>
2023-12-24 02:39:09 -0600

    Added error handler and route to catch bad URI

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-//const createError = require('http-errors');
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const ErrorHandler = require('./middleware/ErrorHandler');
+const errors = require('./middleware/errors/index');
 const models = require('./models/index');
 const routes = require('./routes/index');
 
@@ -30,21 +31,9 @@ app.use(
 	passport.authenticate('refresh', { session: false }),
 	routes.refresh,
 );
-
-/* // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
+app.use((req, res, next) => {
+	next(new errors.BadUri());
 });
+app.use(ErrorHandler);
 
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
-*/
 module.exports = app;
