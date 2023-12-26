7f5abf915fe12d33f2d9dcf392a0101d8381c5ec
Matthew Smart <matthewsmart13@gmail.com>
2023-12-26 16:36:11 -0600

    Added the AuthError to the error index

--- middleware/errors/index.js
+++ middleware/errors/index.js
@@ -2,10 +2,12 @@ const ReqTypeError = require('./ReqTypeError');
 const ResourceError = require('./ResourceError');
 const BadUriError = require('./BadUriError');
 const PermissionError = require('./PermissionError');
+const AuthError = require('./AuthError');
 
 module.exports = {
 	ReqTypeError,
 	ResourceError,
 	BadUriError,
 	PermissionError,
+	AuthError,
 };
