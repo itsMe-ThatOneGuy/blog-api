0c8ce82a041aa38c17d2bf58d09a5d7f50ef490b
Matthew Smart <matthewsmart13@gmail.com>
2023-12-26 00:06:13 -0600

    Added the comment endpoints to postsPublic

--- routes/posts.js
+++ routes/posts.js
@@ -10,6 +10,35 @@ const postsPublic = (router) => {
 		controllers.postController.get_single_post,
 	);
 
+	router.get(
+		'/posts/:postId/comments',
+		idTypeCheck,
+		controllers.commentController.get_post_comments,
+	);
+
+	router.post(
+		'/posts/:postId/comments',
+		idTypeCheck,
+		controllers.commentController.create_comment,
+	);
+
+	router.get(
+		'/posts/:postId/comments/:commentId',
+		idTypeCheck,
+		controllers.commentController.get_single_comment,
+	);
+
+	router.put(
+		'/posts/:postId/comments/:commentId',
+		idTypeCheck,
+		controllers.commentController.update_comment,
+	);
+
+	router.delete(
+		'/posts/:postId/comments/:commentId',
+		idTypeCheck,
+		controllers.commentController.delete_comment,
+	);
 	return router;
 };
 
