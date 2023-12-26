49cf2833eb9e351b55000848317729761ebaf655
Matthew Smart <matthewsmart13@gmail.com>
2023-12-25 23:53:58 -0600

    Created a function to attach specific private endpoints to router

--- routes/private.js
+++ routes/private.js
@@ -1,51 +1,15 @@
 const express = require('express');
-const router = express.Router();
-const controllers = require('../controllers/index');
-const idTypeCheck = require('../middleware/idTypeCheck');
+const PrivateRouter = express.Router();
+const posts = require('./posts');
+const user = require('./user');
 
-//Create Post
-router.post('/posts', controllers.postController.create_post);
+const initPrivateRouter = (router) => {
+	posts.postsPrivate(router);
+	user.userPrivate(router);
 
-//Update post
-router.put(
-	'/posts/:postId',
-	idTypeCheck,
-	controllers.postController.update_post,
-);
+	return router;
+};
 
-//Delete Post
-router.delete(
-	'/posts/:postId',
-	idTypeCheck,
-	controllers.postController.delete_post,
-);
+initPrivateRouter(PrivateRouter);
 
-//update published
-router.put(
-	'/posts/:postId/published',
-	idTypeCheck,
-	controllers.postController.change_published,
-);
-
-//Create a comment
-router.post(
-	'/posts/:postId/comments',
-	idTypeCheck,
-	controllers.commentController.create_comment,
-);
-
-//Update comment
-router.put(
-	'/posts/:postId/comments/:commentId',
-	idTypeCheck,
-	controllers.commentController.update_comment,
-);
-
-//Delete comment
-router.delete(
-	'/posts/:postId/comments/:commentId',
-	idTypeCheck,
-	controllers.commentController.delete_comment,
-);
-
-module.exports = router;
+module.exports = PrivateRouter;
