c94876395f7c2f4e102f07ad87754518487751d3
Matthew Smart <matthewsmart13@gmail.com>
2023-12-28 15:41:49 -0600

    Added EndpointAuth.authentication to the private router

--- routes/private.js
+++ routes/private.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const PrivateRouter = express.Router();
+const endpointAuth = require('./endpointAuth');
 const posts = require('./posts');
 const user = require('./user');
 
 const initPrivateRouter = (router) => {
+	endpointAuth.authentication(router);
 	posts.postsPrivate(router);
 	user.userPrivate(router);
 
