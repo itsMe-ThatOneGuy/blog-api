28f9ce62364863e49f22285414e1f1656773449e
Matthew Smart <matthewsmart13@gmail.com>
2023-12-30 00:34:42 -0600

    Added validation middleware to routes

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,17 @@
 const controllers = require('../controllers/index');
 const idTypeCheck = require('../middleware/idTypeCheck');
+const validator = require('../middleware/validators/index');
 
 const userPublic = (router) => {
 	router.get('/user/:userId', idTypeCheck, controllers.userController.get_user);
 
-	router.post('/user/register', controllers.userController.register_user);
+	router.post(
+		'/user/register',
+		validator.usernameValidator(),
+		validator.passwordValidator(),
+		validator.validate,
+		controllers.userController.register_user,
+	);
 
 	router.post('/user/login', controllers.authController.login_user);
 
