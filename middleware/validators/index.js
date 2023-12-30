ffe6b8ab4ad905d3fba379f7ad341ec191f6e4c4
Matthew Smart <matthewsmart13@gmail.com>
2023-12-30 09:07:59 -0600

    Added new validators to validator index

--- middleware/validators/index.js
+++ middleware/validators/index.js
@@ -1,5 +1,9 @@
 const { validationResult } = require('express-validator');
-const { usernameValidator } = require('./username');
+const usernameValidator = require('./username');
+const passwordValidator = require('./password');
+const titleValidator = require('./title');
+const bodyValidator = require('./body');
+const publishValidator = require('./publish');
 const errors = require('../errors/index');
 
 const validate = (req, res, next) => {
@@ -10,9 +14,14 @@ const validate = (req, res, next) => {
 	const extractedErrors = [];
 	_errors.array().map((err) => extractedErrors.push(err.msg));
 
-	return next(new errors.AuthError(extractedErrors, 400));
+	return next(new errors.ValidationError(extractedErrors, 422));
 };
 
 module.exports = {
 	validate,
+	usernameValidator,
+	passwordValidator,
+	titleValidator,
+	bodyValidator,
+	publishValidator,
 };
