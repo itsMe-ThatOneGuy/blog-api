922c5f5e9991c38bd64405b4525d78c1053d04d2
Matthew Smart <matthewsmart13@gmail.com>
2024-01-09 01:07:24 -0600

    Added userService module

--- services/index.js
+++ services/index.js
@@ -1,9 +1,11 @@
 const postServices = require('./posts');
 const commentServices = require('./comments');
 const authServices = require('./auth');
+const userServices = require('./user');
 
 module.exports = {
 	postServices,
 	commentServices,
 	authServices,
+	userServices,
 };
