cad3c72ae12a8777b2173d7ca7a70b18e1adf880
Matthew Smart <matthewsmart13@gmail.com>
2024-01-08 19:09:51 -0600

    Created auth services module / refactored controller to use module

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,5 @@
-const models = require('../models/index');
 const asyncHandler = require('express-async-handler');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const errors = require('../middleware/errors/index');
-const initPassport = require('../middleware/passport.js');
-const passport = require('passport');
-
-initPassport(passport);
+const services = require('../services/index');
 
 exports.test_auth = (req, res, next) => {
 	try {
@@ -22,61 +15,23 @@ exports.test_auth = (req, res, next) => {
 };
 
 exports.user_Auth = (req, res, next) => {
-	passport.authenticate('jwt', { session: false }, (err, user) => {
-		if (err) {
-			return next(err);
-		}
-		if (!user) {
-			throw new errors.AuthError();
-		}
-		return next();
-	})(req, res, next);
+	services.authServices.userAuth(req, res, next);
 };
 
 exports.token_Auth = (req, res, next) => {
-	passport.authenticate('refresh', function (err, user) {
-		if (err) {
-			return next(err);
-		}
-		return next();
-	})(req, res, next);
+	services.authServices.tokenAuth(req, res, next);
 };
 
 exports.refresh = asyncHandler(async (req, res, next) => {
 	try {
-		const refreshToken = req.cookies.jwt;
-		if (!refreshToken)
-			return next(new errors.AuthError('ACCESS DENIED, NO REFRESH TOKEN', 401));
+		const accessToken = await services.authServices.refresh(req.cookies);
 
-		const decoded = jwt.verify(
-			refreshToken,
-			process.env.JWT_REFRESH_KEY,
-			(err, decoded) => {
-				if (err)
-					return next(new errors.AuthError('INVALID REFRESH TOKEN', 400));
-
-				return decoded;
-			},
-		);
-
-		if (decoded) {
-			const payload = {
-				sub: decoded.sub,
-				username: decoded.username,
-				isAdmin: decoded.isAdmin,
-			};
-
-			const accessToken = jwt.sign(payload, process.env.JWT_TOKEN_KEY, {
-				expiresIn: 120,
-			});
-
-			return res.status(200).json({
-				success: true,
-				status: 200,
-				message: 'ACCESS TOKEN REFRESHED',
-				token: accessToken,
-			});
-		}
+		return res.status(200).json({
+			success: true,
+			status: 200,
+			message: 'ACCESS TOKEN REFRESHED',
+			token: accessToken,
+		});
 	} catch (err) {
 		return next(err);
 	}
@@ -84,37 +39,18 @@ exports.refresh = asyncHandler(async (req, res, next) => {
 
 exports.login_user = asyncHandler(async (req, res, next) => {
 	try {
-		const user = await models.User.findOne({
-			username: req.body.username,
-		}).exec();
-		const password = await bcrypt.compare(req.body.password, user.password);
+		const { accessToken, refreshToken } = await services.authServices.loginUser(
+			req.body,
+		);
 
-		const payload = {
-			sub: user._id,
-			username: user.username,
-			isAdmin: user.isAdmin,
-		};
-
-		if (user && password) {
-			const accessToken = jwt.sign(payload, process.env.JWT_TOKEN_KEY, {
-				expiresIn: 120,
+		return res
+			.cookie('jwt', refreshToken, { httpOnly: true, secure: false })
+			.status(200)
+			.json({
+				success: true,
+				status: 200,
+				token: accessToken,
 			});
-
-			const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_KEY, {
-				expiresIn: '1d',
-			});
-
-			res
-				.cookie('jwt', refreshToken, { httpOnly: true, secure: false })
-				.status(200)
-				.json({
-					success: true,
-					status: 200,
-					token: accessToken,
-				});
-		} else {
-			return next(new errors.AuthError('INVALID USERNAME OR PASSWORD', 401));
-		}
 	} catch (err) {
 		next(err);
 	}
---
+++ services/auth.js
@@ -0,0 +1,85 @@
+const models = require('../models/index');
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const errors = require('../middleware/errors/index');
+const initPassport = require('../middleware/passport.js');
+const passport = require('passport');
+
+initPassport(passport);
+
+exports.userAuth = (req, res, next) => {
+	passport.authenticate('jwt', { session: false }, (err, user) => {
+		if (err) {
+			return next(err);
+		}
+		if (!user) {
+			throw new errors.AuthError();
+		}
+		return next();
+	})(req, res, next);
+};
+
+exports.tokenAuth = (req, res, next) => {
+	passport.authenticate('refresh', function (err, user) {
+		if (err) {
+			return next(err);
+		}
+		return next();
+	})(req, res, next);
+};
+
+exports.loginUser = asyncHandler(async (body) => {
+	const user = await models.User.findOne({
+		username: body.username,
+	}).exec();
+	const password = await bcrypt.compare(body.password, user.password);
+
+	const payload = {
+		sub: user._id,
+		username: user.username,
+		isAdmin: user.isAdmin,
+	};
+
+	if (user && password) {
+		const accessToken = jwt.sign(payload, process.env.JWT_TOKEN_KEY, {
+			expiresIn: 120,
+		});
+
+		const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_KEY, {
+			expiresIn: '1d',
+		});
+
+		return { accessToken, refreshToken };
+	} else {
+		throw new errors.AuthError('INVALID USERNAME OR PASSWORD', 401);
+	}
+});
+
+exports.refresh = asyncHandler(async (cookies) => {
+	const refreshToken = cookies.jwt;
+	if (!refreshToken)
+		throw new errors.AuthError('ACCESS DENIED, NO REFRESH TOKEN', 401);
+
+	const decoded = jwt.verify(
+		refreshToken,
+		process.env.JWT_REFRESH_KEY,
+		(err, decoded) => {
+			if (err) throw new errors.AuthError('INVALID REFRESH TOKEN', 400);
+
+			return decoded;
+		},
+	);
+
+	if (decoded) {
+		const payload = {
+			sub: decoded.sub,
+			username: decoded.username,
+			isAdmin: decoded.isAdmin,
+		};
+
+		return jwt.sign(payload, process.env.JWT_TOKEN_KEY, {
+			expiresIn: 120,
+		});
+	}
+});
