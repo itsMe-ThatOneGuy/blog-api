378615a3fad34a05effed57fa01195e498d8380e
Matthew Smart <matthewsmart13@gmail.com>
2024-01-10 15:14:15 -0600

    Moved post DB logic from post service to post model

--- models/post.js
+++ models/post.js
@@ -1,13 +1,117 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const asyncHandler = require('express-async-handler');
+const errors = require('../middleware/errors/index');
+const CommentModel = require('./comment');
 
 const PostSchema = new Schema({
-	user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-	title: { type: String, required: true },
-	body: { type: String, required: true },
-	comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
-	isPublished: { type: Boolean, default: false },
-	postDate: { type: Date, default: Date.now },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+    isPublished: { type: Boolean, default: false },
+    postDate: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('Post', PostSchema);
+const Post = mongoose.model('Post', PostSchema);
+
+const allPosts = asyncHandler(async () => {
+    return await Post.find({})
+        .populate('user', 'username')
+        .populate({
+            path: 'comments',
+            populate: { path: 'user', select: 'username' },
+        });
+});
+
+const createPosts = asyncHandler(async (user, body) => {
+    if (!user.isAdmin)
+        throw new errors.PermissionError('NOT AUTHORIZED TO MAKE POSTS');
+
+    const post = new Post({
+        user: user.sub,
+        title: body.title,
+        body: body.body,
+    });
+    await post.save();
+
+    return post.populate('user', 'username');
+});
+
+const getSinglePost = asyncHandler(async (params) => {
+    const post = await Post.findById(params.postId)
+        .populate('user', 'username')
+        .populate({
+            path: 'comments',
+            populate: { path: 'user', select: 'username' },
+        });
+
+    if (post === null) throw new errors.ResourceError('COULD NOT FIND POST', 404);
+
+    return post;
+});
+
+const updatePost = asyncHandler(async (params, user, body) => {
+    if (!user.isAdmin)
+        throw new errors.PermissionError('NOT AUTHORIZED TO UPDATE POSTS');
+
+    await getSinglePost(params);
+
+    return await Post.findByIdAndUpdate(
+        params.postId,
+        { $set: { title: body.title, body: body.body } },
+        { new: true },
+    )
+        .populate('user', 'username')
+        .populate({
+            path: 'comments',
+            populate: { path: 'user', select: 'username' },
+        });
+});
+
+const deletePost = asyncHandler(async (params, user) => {
+    if (!user.isAdmin)
+        throw new errors.PermissionError('NOT AUTHORIZED TO DELETE POSTS');
+
+    const post = await getSinglePost(params);
+
+    if (post.comments.length !== 0) {
+        post.comments.forEach(async (comment) => {
+            await CommentModel.findByIdAndDelete(comment._id);
+        });
+    }
+
+    await Post.findByIdAndDelete(params.postId);
+
+    return post;
+});
+
+const changePublished = asyncHandler(async (params, user, body) => {
+    if (!user.isAdmin)
+        throw new errors.PermissionError(
+            'NOT AUTHORIZED TO CHANGE POST PUBLISH STATUS',
+        );
+
+    await getSinglePost(params);
+
+    return await Post.findByIdAndUpdate(
+        params.postId,
+        { $set: { isPublished: body.isPublished } },
+        { new: true },
+    )
+        .populate('user', 'username')
+        .populate({
+            path: 'comments',
+            populate: { path: 'user', select: 'username' },
+        });
+});
+
+module.exports = {
+    Post,
+    allPosts,
+    createPosts,
+    getSinglePost,
+    updatePost,
+    deletePost,
+    changePublished,
+};
--- services/posts.js
+++ services/posts.js
@@ -1,94 +1,26 @@
 const models = require('../models/index');
-const errors = require('../middleware/errors/index');
 const asyncHandler = require('express-async-handler');
 
 exports.allPosts = asyncHandler(async () => {
-	return await models.Post.find({})
-		.populate('user', 'username')
-		.populate({
-			path: 'comments',
-			populate: { path: 'user', select: 'username' },
-		});
+	return await models.PostModel.allPosts();
 });
 
 exports.createPost = asyncHandler(async (user, body) => {
-	if (!user.isAdmin)
-		throw new errors.PermissionError('NOT AUTHORIZED TO MAKE POSTS');
-
-	const post = new models.Post({
-		user: user.sub,
-		title: body.title,
-		body: body.body,
-	});
-	await post.save();
-
-	return post.populate('user', 'username');
+	return await models.PostModel.createPosts(user, body);
 });
 
 exports.getSinglePost = asyncHandler(async (params) => {
-	const post = await models.Post.findById(params.postId)
-		.populate('user', 'username')
-		.populate({
-			path: 'comments',
-			populate: { path: 'user', select: 'username' },
-		});
-
-	if (post === null) throw new errors.ResourceError('COULD NOT FIND POST', 404);
-
-	return post;
+	return await models.PostModel.getSinglePost(params);
 });
 
 exports.updatePost = asyncHandler(async (params, user, body) => {
-	if (!user.isAdmin)
-		throw new errors.PermissionError('NOT AUTHORIZED TO UPDATE POSTS');
-
-	await this.getSinglePost(params);
-
-	return await models.Post.findByIdAndUpdate(
-		params.postId,
-		{ $set: { title: body.title, body: body.body } },
-		{ new: true },
-	)
-		.populate('user', 'username')
-		.populate({
-			path: 'comments',
-			populate: { path: 'user', select: 'username' },
-		});
+	return await models.PostModel.updatePost(params, user, body);
 });
 
 exports.deletePost = asyncHandler(async (params, user) => {
-	if (!user.isAdmin)
-		throw new errors.PermissionError('NOT AUTHORIZED TO DELETE POSTS');
-
-	const post = await this.getSinglePost(params);
-
-	if (post.comments.length !== 0) {
-		post.comments.forEach(async (comment) => {
-			await models.Comment.findByIdAndDelete(comment._id);
-		});
-	}
-
-	await models.Post.findByIdAndDelete(params.postId);
-
-	return post;
+	return await models.PostModel.deletePost(params, user);
 });
 
 exports.changePublished = asyncHandler(async (params, user, body) => {
-	if (!user.isAdmin)
-		throw new errors.PermissionError(
-			'NOT AUTHORIZED TO CHANGE POST PUBLISH STATUS',
-		);
-
-	await this.getSinglePost(params);
-
-	return await models.Post.findByIdAndUpdate(
-		params.postId,
-		{ $set: { isPublished: body.isPublished } },
-		{ new: true },
-	)
-		.populate('user', 'username')
-		.populate({
-			path: 'comments',
-			populate: { path: 'user', select: 'username' },
-		});
+	return await models.PostModel.changePublished(params, user, body);
 });
