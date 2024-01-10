34d68033ff7ae095c04e4802d2b06d44905590f4
Matthew Smart <matthewsmart13@gmail.com>
2024-01-10 15:48:09 -0600

    Moved user DB logic from user services to user model file

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
+const asyncHandler = require('express-async-handler');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+const errors = require('../middleware/errors/index');
 
 const UserSchema = new Schema({
 	username: { type: String, required: true, unique: true },
@@ -7,4 +10,32 @@ const UserSchema = new Schema({
 	isAdmin: { type: Boolean, default: false },
 });
 
-module.exports = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema);
+
+const registerUser = asyncHandler(async (body) => {
+	bcrypt.hash(body.password, 13, async (err, hashedPassword) => {
+		if (err) throw new errors.PermissionError();
+
+		const newUser = new User({
+			username: body.username,
+			password: hashedPassword,
+		});
+
+		await newUser.save();
+	});
+
+	return body.username;
+});
+
+const getUser = asyncHandler(async (params) => {
+	const user = await User.findById(params.userId);
+	if (user === null) throw new errors.ResourceError('USER NOT FOUND');
+
+	return user;
+});
+
+module.exports = {
+	User,
+	registerUser,
+	getUser,
+};
--- services/user.js
+++ services/user.js
@@ -1,24 +1,10 @@
 const models = require('../models/index');
 const asyncHandler = require('express-async-handler');
-const bcrypt = require('bcryptjs');
-const errors = require('../middleware/errors/index');
 
 exports.registerUser = asyncHandler(async (body) => {
-	bcrypt.hash(body.password, 13, async (err, hashedPassword) => {
-		if (err) throw new errors.PermissionError();
-
-		const newUser = new models.User({
-			username: body.username,
-			password: hashedPassword,
-		});
-
-		await newUser.save();
-	});
+	return await models.UserModel.registerUser(body);
 });
 
 exports.getUser = asyncHandler(async (params) => {
-	const user = await models.User.findById(params.userId);
-	if (user === null) throw new errors.ResourceError('USER NOT FOUND');
-
-	return user;
+	return await models.UserModel.getUser(params);
 });
