a31f79480fe2297a52d86ce22498e6ca42ffc463
Matthew Smart <matthewsmart13@gmail.com>
2024-01-11 14:26:05 -0600

    Moved comment DB logic from comment services to model

--- models/comment.js
+++ models/comment.js
@@ -1,5 +1,7 @@
+const asyncHandler = require('express-async-handler');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const errors = require('../middleware/errors/index');
 
 const CommentSchema = new Schema({
 	user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
@@ -7,4 +9,68 @@ const CommentSchema = new Schema({
 	commentDate: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('Comment', CommentSchema);
+const Comment = mongoose.model('Comment', CommentSchema);
+
+const createComment = asyncHandler(async (user, body, post) => {
+	const comment = new Comment({
+		user: user.sub,
+		body: body.body,
+	});
+	await comment.save();
+
+	post.comments.push(comment);
+	const updatedPost = await post.save();
+
+	return { comment, updatedPost };
+});
+
+const getSingleComment = asyncHandler(async (params) => {
+	const comment = await Comment.findById(params.commentId).populate(
+		'user',
+		'username',
+	);
+	if (comment === null) throw new errors.ResourceError('COMMENT NOT FOUND');
+
+	return comment;
+});
+
+const updateComment = asyncHandler(async (params, user, body) => {
+	const comment = await getSingleComment(params);
+
+	if (comment.user.id !== user.sub)
+		throw new errors.PermissionError('NOT AUTHORIZED TO UPDATE COMMENT');
+
+	return await Comment.findByIdAndUpdate(
+		params.commentId,
+		{ $set: { body: body.body } },
+		{ new: true },
+	).populate('user', 'username');
+});
+
+const deleteComment = asyncHandler(async (params, user) => {
+	const comment = await getSingleComment(params);
+
+	if (comment.user.id !== user.sub || !user.isAdmin)
+		throw new errors.PermissionError('NOT AUTHORIZED TO DELETE COMMENT');
+
+	await Comment.findByIdAndDelete(params.commentId);
+
+	return comment;
+});
+
+const deleteAllPostComments = asyncHandler(async (post) => {
+	if (post.comments.length !== 0) {
+		post.comments.forEach(async (comment) => {
+			await Comment.findByIdAndDelete(comment._id);
+		});
+	}
+});
+
+module.exports = {
+	Comment,
+	createComment,
+	getSingleComment,
+	updateComment,
+	deleteComment,
+	deleteAllPostComments,
+};
--- services/comments.js
+++ services/comments.js
@@ -1,62 +1,23 @@
 const models = require('../models/index');
 const errors = require('../middleware/errors/index');
-const postServices = require('./posts');
 const asyncHandler = require('express-async-handler');
 
 exports.createComment = asyncHandler(async (params, user, body) => {
-	const post = await postServices.getSinglePost(params);
-
-	const comment = new models.Comment({
-		user: user.sub,
-		body: body.body,
-	});
-	await comment.save();
-
-	post.comments.push(comment);
-	const updatedPost = await post.save();
-
-	return { comment, updatedPost };
+	const post = await models.PostModel.getSinglePost(params);
+	return await models.CommentModel.createComment(user, body, post);
 });
 
 exports.getSingleComment = asyncHandler(async (params) => {
-	const comment = await models.Comment.findById(params.commentId).populate(
-		'user',
-		'username',
-	);
-	if (comment === null) throw new errors.ResourceError('COMMENT NOT FOUND');
-
-	return comment;
+	return await models.CommentModel.getSingleComment(params);
 });
 
 exports.updateComment = asyncHandler(async (params, user, body) => {
-	const comment = await this.getSingleComment(params);
-
-	if (comment.user.id !== user.sub)
-		throw new errors.PermissionError('NOT AUTHORIZED TO UPDATE COMMENT');
-
-	return await models.Comment.findByIdAndUpdate(
-		params.commentId,
-		{ $set: { body: body.body } },
-		{ new: true },
-	).populate('user', 'username');
+	return await models.CommentModel.updateComment(params, user, body);
 });
 
 exports.deleteComment = asyncHandler(async (params, user) => {
-	const comment = await this.getSingleComment(params);
+	const comment = await models.CommentModel.deleteComment(params, user);
+	const post = await models.PostModel.updatePostComments(params);
 
-	if (comment.user.id !== user.sub || !user.isAdmin)
-		throw new errors.PermissionError('NOT AUTHORIZED TO DELETE COMMENT');
-
-	await models.Comment.findByIdAndDelete(params.commentId);
-
-	await models.Post.findOneAndUpdate(
-		{ _id: params.postId },
-		{
-			$pull: {
-				comments: params.commentId,
-			},
-		},
-	);
-
-	return comment;
+	return { comment, post };
 });
