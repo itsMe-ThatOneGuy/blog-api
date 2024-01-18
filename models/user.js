0e7c8a7b279b4641ed32fe738a58ce4996079e5e
Matthew Smart <matthewsmart13@gmail.com>
2024-01-18 11:42:01 -0600

    Moved business logic from user model to user services

--- models/user.js
+++ models/user.js
@@ -1,8 +1,5 @@
-const asyncHandler = require('express-async-handler');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require('bcryptjs');
-const errors = require('../middleware/errors/index');
 
 const UserSchema = new Schema({
 	username: { type: String, required: true, unique: true },
@@ -10,37 +7,4 @@ const UserSchema = new Schema({
 	isAdmin: { type: Boolean, default: false },
 });
 
-const User = mongoose.model('User', UserSchema);
-
-const registerUser = asyncHandler(async (username, password) => {
-	const salt = await bcrypt.genSalt(13);
-	const hashedPassword = await bcrypt.hash(password, salt);
-
-	const newUser = new User({
-		username: username,
-		password: hashedPassword,
-	});
-
-	return await newUser.save();
-});
-
-const getUserById = asyncHandler(async (id) => {
-	const user = await User.findById(id);
-	if (user === null) throw new errors.ResourceError('USER NOT FOUND');
-
-	return user;
-});
-
-const getUserByName = asyncHandler(async (username) => {
-	const user = await User.findOne({ username: username });
-	if (user === null) throw new errors.ResourceError('USER NOT FOUND');
-
-	return user;
-});
-
-module.exports = {
-	User,
-	registerUser,
-	getUserById,
-	getUserByName,
-};
+module.exports = mongoose.model('User', UserSchema);
--- services/user.js
+++ services/user.js
@@ -1,10 +1,36 @@
-const models = require('../models/index');
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const errors = require('../middleware/errors/index');
+const { UserModel } = require('../models/index');
 
-exports.registerUser = asyncHandler(async (username, password) => {
-	return await models.UserModel.registerUser(username, password);
+const registerUser = asyncHandler(async (username, password) => {
+	const salt = await bcrypt.genSalt(13);
+	const hashedPassword = await bcrypt.hash(password, salt);
+
+	const newUser = new UserModel({
+		username: username,
+		password: hashedPassword,
+	});
+
+	return await newUser.save();
 });
 
-exports.getUser = asyncHandler(async (id) => {
-	return await models.UserModel.getUser(id);
+const getUserById = asyncHandler(async (id) => {
+	const user = await UserModel.findById(id);
+	if (user === null) throw new errors.ResourceError('USER NOT FOUND');
+
+	return user;
 });
+
+const getUserByName = asyncHandler(async (username) => {
+	const user = await UserModel.findOne({ username: username });
+	if (user === null) throw new errors.ResourceError('USER NOT FOUND');
+
+	return user;
+});
+
+module.exports = {
+	registerUser,
+	getUserById,
+	getUserByName,
+};
